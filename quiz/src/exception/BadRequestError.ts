9d0389bbd9b340c7e519b02a514ee8d89c4e038c
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2023-12-15 22:12:47 -0300

    feat: adicionando tratamento de errors personalizado

---
+++ quiz/src/exception/BadRequestError.ts
@@ -0,0 +1,8 @@
+import { ApiError } from './apiError';
+
+
+export class BadRequestError extends ApiError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
---
+++ quiz/src/exception/UnauthorizedError.ts
@@ -0,0 +1,8 @@
+import { ApiError } from './apiError';
+
+
+export class UnauthorizedError extends ApiError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
