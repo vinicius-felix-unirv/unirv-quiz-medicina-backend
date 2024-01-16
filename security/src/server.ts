97f1bff5088e335674b7367b3db0d9143b36d363
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-01-15 22:00:46 -0300

    fix: apagando os outros microservices

--- security/src/server.ts
+++
@@ -1,9 +0,0 @@
-import Express from 'express';
-
-const app = Express();
-
-app.use(Express.json());
-
-app.get('/', (req, res) => res.json({message: 'deu bom outra vez'}));
-
-app.listen(3001, () => console.log('rodando...'));
