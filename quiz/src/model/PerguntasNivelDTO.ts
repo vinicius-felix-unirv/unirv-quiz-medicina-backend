6741d5b0f2975d42314a26edaadcbc8cee808f3f
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-01-22 18:12:30 -0300

    feat: model de perguntasnivel criada

---
+++ quiz/src/model/PerguntasNivelDTO.ts
@@ -0,0 +1,33 @@
+import { perguntasnivel } from '@prisma/client';
+
+export interface IPerguntasNivelDTO{
+
+    id?: number;
+    nivel: number;    
+    pontuacao: number;
+    tempo: number;   
+}
+
+export class PerguntasNivel{
+
+  private id?: number;
+  private nivel: number;    
+  private pontuacao: number;
+  private tempo: number;   
+  
+  constructor(perguntasNivel: IPerguntasNivelDTO | perguntasnivel){
+
+    this.id = perguntasNivel.id;
+    this.nivel = perguntasNivel.nivel;
+    this.pontuacao = perguntasNivel.pontuacao;
+    this.tempo = perguntasNivel.tempo;
+  }
+
+  getId(): number {  return this.id!;  }
+
+  getNivel(): number { return this.nivel; }
+  
+  getPontuacao(): number { return this.pontuacao; }
+  
+  getTempo(): number { return this.tempo; }
+}
