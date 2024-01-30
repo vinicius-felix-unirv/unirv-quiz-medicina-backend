1e2c9748705274380b57b1b436badf621cfc7540
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-01-30 16:32:15 -0300

    fix: apagando codigo inuti

--- quiz/src/routes/progressoPerguntasRoutes.ts
+++ quiz/src/routes/progressoPerguntasRoutes.ts
@@ -8,10 +8,8 @@ const progressoPerguntasRoutes = Router();
 const progressoPerguntasController = new ProgressoPerguntasController();
 
 progressoPerguntasRoutes.get('/progressoPerguntas/:id', isAuthenticated, progressoPerguntasController.getAllProgressoPergByUsuario);
-// progressoPerguntasRoutes.get('/perguntas', isAuthenticated, progressoPerguntasController.getAllPergunta);
 progressoPerguntasRoutes.post('/progressoPerguntas', isAuthenticated, progressoPerguntasController.postProgressoPerg);
-// progressoPerguntasRoutes.put('/perguntas/:id', isAuthenticated, progressoPerguntasController.putPergunta);
-// progressoPerguntasRoutes.put('/perguntas/status/:id', isAuthenticated, progressoPerguntasController.putStatusPergunta);
+
 
 export { progressoPerguntasRoutes };
 
