44caea61f49ecdb3047e399c51f7317605a1733d
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-02-17 02:06:36 -0300

    fix: arrumando a logica

--- quiz/src/service/ProgressoPerguntasService.ts
+++ quiz/src/service/ProgressoPerguntasService.ts
@@ -2,11 +2,23 @@ import { Service } from 'typedi';
 import { ProgressoPerguntasDTO } from '../model/ProgressoPerguntasDTO';
 import progressoPerguntasRepository from '../repository/progressoPerguntasRepository';
 import { BadRequestError } from '../exception/BadRequestError';
+import usuariosRepository from '../repository/usuariosRepository';
+import perguntasRepository from '../repository/perguntasRepository';
+import { NotFoundError } from '../exception/NotFoundError';
 
 @Service()
 export class ProgressoPerguntasService{
 
   async createProgressoPerg(progressoPergunta: ProgressoPerguntasDTO): Promise<ProgressoPerguntasDTO>{
+
+    const [usuariosIdExists, perguntaIdExists] = await Promise.all(
+      [
+        usuariosRepository.getUsuarioById(progressoPergunta.getUsuariosId()),
+        perguntasRepository.getPerguntaById(progressoPergunta.getPerguntasId())
+      ]
+    );
+    
+    if(!usuariosIdExists || !perguntaIdExists) throw new NotFoundError('Usuario or Pergunta not found');
 
     const progressoPergByUsuario = await progressoPerguntasRepository.getProgressoPerguntasByUsuario(progressoPergunta.getUsuariosId());
 
