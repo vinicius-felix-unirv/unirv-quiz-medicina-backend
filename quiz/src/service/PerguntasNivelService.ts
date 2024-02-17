b31f16a8da4dd7fb4b3ebe46ca2aad56c72058b0
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-02-17 01:43:04 -0300

    fix: arrumando a logica

--- quiz/src/service/PerguntasNivelService.ts
+++ quiz/src/service/PerguntasNivelService.ts
@@ -42,7 +42,7 @@ export class PerguntasNivelService{
 
     const perguntasNivelAlreadyExists = await perguntasNivelRepository.getPerguntasNivelByNivel(perguntasNivel.getNivel());
 
-    if(!(perguntasNivelAlreadyExists?.id === id)) throw new BadRequestError('PerguntasNivel already exists');
+    if(perguntasNivelAlreadyExists != null && perguntasNivelAlreadyExists?.id != id) throw new BadRequestError('PerguntasNivel already exists');
 
     const updatedPerguntasNivel = await perguntasNivelRepository.alterPerguntasNivel(id, perguntasNivel);
 
