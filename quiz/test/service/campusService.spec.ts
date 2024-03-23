bfbbb9a07f63c3c155891cd61947686f641c7a00
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-03-22 21:47:24 -0300

    feat: add testes para a função getALlCampusByUserId

--- quiz/test/service/campusService.spec.ts
+++ quiz/test/service/campusService.spec.ts
@@ -6,28 +6,22 @@ import { CampusService } from '../../src/service/CampusService';
 
 jest.mock('../../src/repository/campusRepository');
 
-const orignGetCampusByUserId = campusRepository.getCampusByUserId;
+const campusMock = {
+    id: 1,
+    curso: 'string',
+    turma: 'string',
+    periodo: 3,
+    nome: 'Pedro',
+    usuariosid: 3
+};
+
+const campusService = new CampusService();
 
 describe('testando função getCampusByUserId', () => {
-
-    const campusMock = {
-        id: 1,
-        curso: 'string',
-        turma: 'string',
-        periodo: 3,
-        nome: 'Pedro',
-        usuariosid: 3
-    };
-
-    beforeEach(() => {
-        campusRepository.getCampusByUserId = orignGetCampusByUserId;
-    });
 
     it('deve retorne um campusDTO', async () => {
         
         campusRepository.getCampusByUserId = jest.fn().mockResolvedValueOnce(campusMock);
-
-        const campusService = new CampusService();
 
         const result = await campusService.getCampusByUserId(1);
         
@@ -38,11 +32,34 @@ describe('testando função getCampusByUserId', () => {
     it('deve retornar um NotFoundError',  () => {
 
         campusRepository.getCampusByUserId = jest.fn().mockResolvedValueOnce(null);
-        
-        const campusService = new CampusService();
 
         expect(async () => campusService.getCampusByUserId(1)).rejects.toThrow(NotFoundError);        
 
     });
+
+});
+
+describe('testando a função getAllCampusByUserId', () => {
+
+    it('deve retornar uma lista de campusDTO', async () => {
+
+        campusRepository.getAllCampusByUserId = jest.fn().mockResolvedValueOnce([campusMock, campusMock]);
+
+        const campusList = await campusService.getAllCampusByUserId(3);
+
+        expect(campusList).toEqual([campusMock, campusMock]);
+        expect(campusList).toHaveLength(2);
+    });
+
+    it('deve retornar uma lista vazia', async () => {
+
+        campusRepository.getAllCampusByUserId = jest.fn().mockResolvedValueOnce([]);
+
+        const campusList = await campusService.getAllCampusByUserId(3);
+
+        expect(campusList).toEqual([]);
+        expect(campusList).toHaveLength(0);
+    });
+
 
 });
