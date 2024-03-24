3de89196adaebe63d692daef471daef3babef53d
admilson <bittencourta942@gmail.com>
2024-03-24 19:47:11 -0300

    feat: add teste para a função getCategoriaId

--- quiz/test/service/categoriasService.spec.ts
+++ quiz/test/service/categoriasService.spec.ts
@@ -1,4 +1,5 @@
 import { BadRequestError } from '../../src/exception/BadRequestError';
+import { NotFoundError } from '../../src/exception/NotFoundError';
 import { CategoriasDTO } from '../../src/model/CategoriasDTO';
 import categoriasRepository from '../../src/repository/categoriasRepository';
 import { CategoriasService } from '../../src/service/CategoriasService';
@@ -37,4 +38,28 @@ describe('testando a função saveCategoria', () => {
     });
 });
 
+describe('testando a função getCategoriaId', () => {
+
+    it('deve retornar uma intancia de CategoriasDTO', async () => {
+
+        categoriasRepository.getCategoriaId = jest.fn().mockResolvedValueOnce(categoriaMock);
+
+        const newCategoria = await categoriasService.getCategoriaId(3);
+
+        expect(newCategoria).toBeInstanceOf(CategoriasDTO);
+        expect(newCategoria).toEqual(categoriaMock);
+        
+    });
+
+    it('deve retornar um NotFoundError',  () => {
+
+        categoriasRepository.getCategoriaId = jest.fn().mockResolvedValueOnce(null);
+
+        expect(async () => {
+            await categoriasService.getCategoriaId(3);
+        }).rejects.toThrow(NotFoundError);
+        
+    });
+
+});
 
