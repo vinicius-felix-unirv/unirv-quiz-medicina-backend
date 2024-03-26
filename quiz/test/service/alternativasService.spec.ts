86c0f93cbbacd704d1afe896ba5c3817938d21a1
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-03-26 17:26:23 -0300

    feat: add testes para a função alternativasCanBePersistent

---
+++ quiz/test/service/alternativasService.spec.ts
@@ -0,0 +1,48 @@
+import { BadRequestError } from '../../src/exception/BadRequestError';
+import { AllAlternativasDTO } from '../../src/model/AlternativasDTO';
+import alternativasRepository from '../../src/repository/alternativasRepository';
+import { AlternativasService } from '../../src/service/AlternativasService';
+
+jest.mock('../../src/repository/alternativasRepository');
+
+const alternativasService = new AlternativasService();
+const alternativaMock = {
+    id: 3,
+    perguntasid: 2,
+    resposta: 'string',
+    pathimage: 'string',
+    correta: false,
+};
+
+describe('testando a função alternativasCanBePersistent', () => {
+
+    it('não deve retornar uma exeção', () => {
+
+        alternativasRepository.gatAllAternativasByPerguntaId = jest.fn().mockResolvedValueOnce([]);
+
+        expect( async () => {
+            await alternativasService.alternativasCanBePersistent(new AllAlternativasDTO([alternativaMock]));
+        }).not.toThrow();
+
+    });
+
+    it('deve retornar um BadRequestError com a mensagem: limit of alternativa exceeded', async () => {
+
+        alternativasRepository.gatAllAternativasByPerguntaId = jest.fn().mockResolvedValueOnce([alternativaMock, alternativaMock, alternativaMock, alternativaMock, alternativaMock]);
+
+        await expect(alternativasService.alternativasCanBePersistent(new AllAlternativasDTO([alternativaMock]))).rejects.toMatchObject({
+            constructor: BadRequestError,
+            message: 'limit of alternativa exceeded'
+          });
+    });
+
+    it('deve retornar um BadRequestError com a mensagem: Alternativa already exists', async () => {
+
+        alternativasRepository.gatAllAternativasByPerguntaId = jest.fn().mockResolvedValueOnce([alternativaMock]);      
+        
+        await expect(alternativasService.alternativasCanBePersistent(new AllAlternativasDTO([alternativaMock]))).rejects.toMatchObject({
+            constructor: BadRequestError,
+            message: 'Alternativa already exists'
+          });
+    });
+});
