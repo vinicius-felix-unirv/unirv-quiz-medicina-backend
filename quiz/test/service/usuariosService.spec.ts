6405e483ece5559dbe556d512a75bf736a448159
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-03-31 15:26:12 -0300

    feat: add testes para a função alterPassword

--- quiz/test/service/usuariosService.spec.ts
+++ quiz/test/service/usuariosService.spec.ts
@@ -1,4 +1,3 @@
-// import { hash } from 'bcryptjs';
 import { BadRequestError } from '../../src/exception/BadRequestError';
 import { NotFoundError } from '../../src/exception/NotFoundError';
 import { UsuarioDTO } from '../../src/model/UsuariosDTO';
@@ -82,4 +81,84 @@ describe('testando a função saveUsuario', () => {
         });
 
     });
+});
+
+describe('testando a função alterUsuario', () => {
+
+    it('deve retornar uma instancia de UsuarioDTO alterada', async () => {
+
+        usuariosRepository.getUsuarioById = jest.fn().mockResolvedValueOnce(user);
+
+        usuariosRepository.getUsuarioByEmail = jest.fn().mockResolvedValueOnce(null);
+
+        usuariosRepository.updateusuario = jest.fn().mockResolvedValueOnce(user);
+
+        const newUser = await usuarioService.alterUsuario(2, new UsuarioDTO(user));
+
+        expect(newUser).toBeInstanceOf(UsuarioDTO);
+        expect(newUser).toEqual(user);
+    });
+
+    it('deve retornar um NotFoundError com a mensagem: Usuario nao encontrado', async () => {
+
+        usuariosRepository.getUsuarioById = jest.fn().mockResolvedValueOnce(null);
+
+        await expect(usuarioService.alterUsuario(3, new UsuarioDTO(user))).rejects.toMatchObject({
+            constructor: NotFoundError,
+            message: 'Usuario nao encontrado'
+        });
+    });
+
+    it('deve retornar um BadRequestError com a mensagem: Usuario ja existe', async () => {
+
+        usuariosRepository.getUsuarioById = jest.fn().mockResolvedValueOnce(user);
+
+        usuariosRepository.getUsuarioByEmail = jest.fn().mockResolvedValueOnce([user]);
+
+        await expect(usuarioService.alterUsuario(3, new UsuarioDTO(user))).rejects.toMatchObject({
+            constructor: BadRequestError,
+            message: 'Usuario ja existe'
+        });
+    });
+});
+
+describe('testando a função alterPassword', () => {
+
+    
+
+    it('deve retornar um NotFoundError com a mensagem: Usuario nao encontrado', async () => {
+
+        usuariosRepository.getUsuarioById = jest.fn().mockResolvedValueOnce(null);
+
+        await expect(usuarioService.alterPassword(2, 'ueueueu')).rejects.toMatchObject({
+            constructor: NotFoundError,
+            message: 'Usuario nao encontrado'
+        });
+        
+    });
+
+    it('deve executar a função usuariosRepository.updateSenhaUsuario apenas uma vez', async () => {
+
+        usuariosRepository.getUsuarioById = jest.fn().mockResolvedValueOnce(user);
+        usuariosRepository.updateSenhaUsuario = jest.fn().mockImplementation(() => {
+            console.log('updateSenhaUsuario foi chamado');
+        });
+
+        await usuarioService.alterPassword(2, 'ueueueu');
+
+        expect(usuariosRepository.updateSenhaUsuario).toHaveBeenCalled();
+        expect(usuariosRepository.updateSenhaUsuario).toHaveBeenCalledTimes(1);
+
+    });
+
+    it('não deve retornar uma exceção', () => {
+
+        usuariosRepository.getUsuarioById = jest.fn().mockResolvedValueOnce(user);
+        usuariosRepository.updateSenhaUsuario = jest.fn();
+
+        expect(async () => await usuarioService.alterPassword(2, 'ueueueu')).not.toThrow();
+
+    });
+
+    
 });
