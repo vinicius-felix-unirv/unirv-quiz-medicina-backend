ecf39908da2fd8471f707cc41bd5875e3d46926f
Pedro-Bittencourt-Costa <pedro.b.costa@academico.unirv.edu.br>
2024-06-26 16:47:04 -0300

    fix: retirando a autenticação

--- quiz/src/routes/perguntasRoutes.ts
+++ quiz/src/routes/perguntasRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { PerguntasController } from '../controller/PerguntasController';
-import { isAuthenticated } from '../middlewares/isAuthenticated';
+// import { isAuthenticated } from '../middlewares/isAuthenticated';
 
 
 const perguntasRoutes = Router();
@@ -9,8 +9,8 @@ const perguntasController = new PerguntasController();
 perguntasRoutes.get('/perguntas/:id',  perguntasController.getPergunta);
 perguntasRoutes.get('/usuarios/:usuariosid/quiz/:id/categorias/:categoriasid/perguntas/:skip/:take',  perguntasController.getAllPerguntasByQuizId);
 perguntasRoutes.get('/quiz/:id/categorias/:categoriasid/perguntas/:skip/:take',  perguntasController.getAllPerguntasByQuizIdForProf);
-perguntasRoutes.post('/perguntas', isAuthenticated, perguntasController.postPergunta);
-perguntasRoutes.put('/perguntas/:id', isAuthenticated, perguntasController.putPergunta);
-perguntasRoutes.put('/perguntas/:id/status', isAuthenticated, perguntasController.putStatusPergunta);
+perguntasRoutes.post('/perguntas', perguntasController.postPergunta);
+perguntasRoutes.put('/perguntas/:id',  perguntasController.putPergunta);
+perguntasRoutes.put('/perguntas/:id/status',  perguntasController.putStatusPergunta);
 
 export { perguntasRoutes };
